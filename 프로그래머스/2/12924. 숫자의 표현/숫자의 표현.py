9c4ac46c7a9cc8ec6a2708f4b8e769c9d2a364d8
Heeryeong <56327334+qwasd34@users.noreply.github.com>
2024-04-24 20:38:23 +0900

    [level 2] Title: 숫자의 표현, Time: 6.17 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,15 @@
+def solution(n):
+    n_list=list(range(1,n+1))
+    
+    cnt=0
+    for i in range(n):
+        sum=0
+        for j in range(i,len(n_list)):
+            sum+=n_list[j]
+            if sum== n:
+                cnt+=1
+                break
+            elif sum > n:
+                break
+    return cnt
+    

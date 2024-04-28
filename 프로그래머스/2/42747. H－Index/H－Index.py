5e1013e14052721498af289a50f8187058a6ed66
Heeryeong <56327334+qwasd34@users.noreply.github.com>
2024-04-29 01:21:05 +0900

    Update H－Index.py

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,14 +1,24 @@
-def solution(citations):
-    citations=sorted(citations,reverse=True)
+# def solution(citations):
+#     citations=sorted(citations,reverse=True)
     
-    answer,cnt=0,0
-    for i in range(len(citations)):
-        if i>=citations[i]:
+#     answer,cnt=0,0
+#     for i in range(len(citations)):
+#         if i>=citations[i]:
+#             answer=i
+#             cnt=cnt+1
+#             break
+#     if cnt==0:
+#         answer=len(citations)
+#     return answer
+
+def solution(citations):
+    citations.sort()
+    
+    answer=0
+    for i in range(1,len(citations)+1):
+        min_c=citations[-i]
+        if min_c>=i:
             answer=i
-            cnt=cnt+1
-            break
-    if cnt==0:
-        answer=len(citations)
     return answer
 
 

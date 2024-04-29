4ba35ac85ecb4df480829172621358177062a3fd
Heeryeong <56327334+qwasd34@users.noreply.github.com>
2024-04-29 16:41:38 +0900

    Update H－Index.py

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -12,12 +12,12 @@
 #     return answer
 
 def solution(citations):
-    citations.sort()
+    citations.sort()   # [0,1,3,5,6]
     
     answer=0
     for i in range(1,len(citations)+1):  
         num=citations[-i]  
-        if num>=i:
+        if num>=i:    #i=3일때, 뒤에서3번째 숫자까지 3번이상인용되어야 h-index=3, i=4일때 citations[-4]=1로 만족x
             answer=i
     return answer
 

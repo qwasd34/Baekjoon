47ad4baa7d53808707177ba9865c950393dbea33
Heeryeong <56327334+qwasd34@users.noreply.github.com>
2024-05-04 04:41:05 +0900

    [level 2] Title: 올바른 괄호, Time: 14.78 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,40 @@
+# def solution(s):
+#     stack=list(s)
+#     rc,lc=0,0
+#     for i in range(len(stack)):
+#         if rc==lc:
+#             rc,lc=0,0
+#         if stack[-1]==')':
+#             rc+=1
+#             stack.pop()
+    
+#         elif rc!=0 and stack[-1]=='(':
+#             lc+=1
+#             stack.pop()
+
+#         else:
+#             return False
+#     if rc==lc:
+#         return True
+#     else: return False
+
+
+def solution(s):
+    st=list(s)
+    #print(st)
+    rg=len(st)-1
+    R=0
+    for i in range(len(st)):
+        if st[-1]==')':
+            st.pop()
+            R+=1
+        elif R!=0 and st[-1]=='(':
+            st.pop()
+            R-=1
+        else:
+            return False
+    if R>0:
+        return False
+    return True
+
+

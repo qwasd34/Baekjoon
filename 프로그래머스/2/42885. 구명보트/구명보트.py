896d532e792e5aba236d8347dfddf08ae328c0f6
Heeryeong <56327334+qwasd34@users.noreply.github.com>
2024-05-09 12:00:53 +0900

    [level 2] Title: 구명보트, Time: 11.14 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,17 +1,31 @@
+# def solution(people, limit):
+#     people.sort()
+#     cnt=0
+#     i=0 #가장 가벼운 사람
+#     h=len(people)-1 #가장 무거운사람
+#     while i<=h:
+#         cnt+=1
+#         if people[i]+people[h]<=limit:
+#             i+=1
+#         h-=1
+#     return cnt
+
+from collections import deque
+
 def solution(people, limit):
-    answer = 0
     people.sort()
-    cnt=0
-    i=0 #가장 가벼운 사람
-    h=len(people)-1 #가장 무거운사람
-    while i<=h:
-        cnt+=1
-        if people[i]+people[h]<=limit:
-            i+=1
-        h-=1
-    return cnt
-
-
+    dq=deque(people)
+    answer=0
+    while dq:
+        if len(dq)>=2 and dq[-1]+dq[0]<=limit:
+            dq.pop()
+            dq.popleft()
+            answer+=1
+        else:
+            dq.pop()
+            answer+=1
+    return answer
+        
             
     
 
